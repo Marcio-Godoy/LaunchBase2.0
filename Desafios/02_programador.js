a64d7c1135e7d7f6ce8ab7ad1be4f95a4578a5dc
Marcio Godoy <marciogodoybts97@gmail.com>
2020-07-02 01:41:58 -0300

    Update 02_programador.js

--- Desafios/02_programador.js
+++ Desafios/02_programador.js
@@ -12,4 +12,4 @@ const programador = {
     ]
 }
 
-console.log('O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}')
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade`)
